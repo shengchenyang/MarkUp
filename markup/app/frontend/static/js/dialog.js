51114bae1178a81bf5cc926740e7900e1f5ca59c
shengchenyang <15538221825@163.com>
2023-10-26 17:23:11 +0800

    禁止 f5

--- markup/app/frontend/static/js/dialog.js
+++ markup/app/frontend/static/js/dialog.js
@@ -1,9 +1,15 @@
-function showPopup() {
+document.addEventListener('keydown', function (event){
+    if (event.key === 'F5') {
+        event.preventDefault();
+    }
+});
+
+function showPopup(){
     document.getElementById('overlay').style.display = 'block';
     document.getElementById('popup').style.display = 'block';
 }
 
-function hidePopup() {
+function hidePopup(){
     document.getElementById('overlay').style.display = 'none';
     document.getElementById('popup').style.display = 'none';
 }
