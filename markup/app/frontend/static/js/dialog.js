8402602b03d7ee8525fbae4c423b8d6b13acf4e2
shengchenyang <15538221825@163.com>
2023-11-13 23:35:34 +0800

    修改为右侧 iframe 加载网页

--- markup/app/frontend/static/js/dialog.js
+++ markup/app/frontend/static/js/dialog.js
@@ -1,20 +1,24 @@
-document.addEventListener('keydown', function (event){
+document.addEventListener('keydown', function (event) {
     if (event.key === 'F5') {
         event.preventDefault();
     }
 });
 
-function showPopup(){
+const iframe = document.querySelector('iframe');
+iframe.setAttribute('sandbox', 'allow-same-origin allow-scripts allow-popups allow-forms allow-top-navigation-by-user-activation');
+iframe.setAttribute('src', '');
+
+function showPopup() {
     document.getElementById('overlay').style.display = 'block';
     document.getElementById('popup').style.display = 'block';
 }
 
-function hidePopup(){
+function hidePopup() {
     document.getElementById('overlay').style.display = 'none';
     document.getElementById('popup').style.display = 'none';
 }
 
 Object.assign(window, {
-  showPopup: showPopup,
-  hidePopup: hidePopup,
+    showPopup: showPopup,
+    hidePopup: hidePopup,
 });
