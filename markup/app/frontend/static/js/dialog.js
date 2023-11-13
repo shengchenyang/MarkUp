029e99c4726a6c3b7c96310ccd96833cbb35b4b5
shengchenyang <15538221825@163.com>
2023-11-13 13:55:09 +0800

    修改为在 frame 中展示网页 && 格式化

--- markup/app/frontend/static/js/dialog.js
+++ markup/app/frontend/static/js/dialog.js
@@ -1,20 +1,24 @@
-document.addEventListener('keydown', function (event){
+document.addEventListener('keydown', function (event) {
     if (event.key === 'F5') {
         event.preventDefault();
     }
 });
 
-function showPopup(){
+const iframe = document.querySelector('iframe');
+iframe.setAttribute('sandbox', 'allow-same-origin allow-scripts allow-popups allow-forms');
+iframe.setAttribute('src', ''); //必须设置src为空 否则不会生效。
+
+function showPopup() {
     document.getElementById('overlay').style.display = 'block';
     document.getElementById('popup').style.display = 'block';
 }
 
-function hidePopup(){
+function hidePopup() {
     document.getElementById('overlay').style.display = 'none';
     document.getElementById('popup').style.display = 'none';
 }
 
 Object.assign(window, {
-  showPopup: showPopup,
-  hidePopup: hidePopup,
+    showPopup: showPopup,
+    hidePopup: hidePopup,
 });
